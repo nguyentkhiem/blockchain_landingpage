a3575cfc2eca3bca04b844777fa760eef59d764c
khiemnt <nguyentkhiem96@gmail.com>
2024-08-15 23:48:07 +0700

    sonala

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Gochi_Hand } from "next/font/google";
 import "@/styles/globals.scss";
+import { ConfigProvider, ThemeConfig } from "antd";
 
 const GochiHand = Gochi_Hand({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-gochi",
 });
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    fontFamily: "var(--font-gochi)",
+  },
+};
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en" className={GochiHand.className}>
@@ -17,7 +25,9 @@ const RootLayout = ({ children }: React.PropsWithChildren) => (
         margin: 0,
       }}
     >
-      <AntdRegistry>{children}</AntdRegistry>
+      <AntdRegistry>
+        <ConfigProvider theme={theme}>{children}</ConfigProvider>
+      </AntdRegistry>
     </body>
   </html>
 );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button, Col, Row, Typography } from "antd";
-import { DownloadOutlined } from "@ant-design/icons";
 import home from "./home.module.scss";
 import Image from "next/image";
 
@@ -12,7 +11,13 @@ export default function Home() {
     <div className={home.container}>
       <div className={home.content1}>
         <div className={home.imageTitle}>
-          <Image width={300} height={133} src="/ROXY.svg" alt="" />
+          <Image
+            className={home.imageTitleI}
+            width={400}
+            height={133}
+            src="/ROXY.svg"
+            alt=""
+          />
         </div>
         <Title level={3} className={home.description}>
           Powered by Solana
@@ -28,15 +33,15 @@ export default function Home() {
           >
             <Image
               className={home.imageCloud}
-              width={426}
-              height={188}
+              width={320}
+              height={120}
               src="/cloud.svg"
               alt=""
             />
             <Image
               className={home.imageCat}
-              width={697}
-              height={188}
+              width={380}
+              height={250}
               src="/cat.svg"
               alt=""
             />
@@ -44,57 +49,88 @@ export default function Home() {
           <Col xs={24} sm={12} md={12} lg={12} xl={12}>
             <Row>
               <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                <Title level={3}>Solana’s most adorable cat</Title>
+                <Title className={home.titleSolana}>
+                  Solana’s most adorable cat
+                </Title>
               </Col>
             </Row>
             <Row>
-              <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+              <Col
+                className={home.boxBtnBatch1}
+                xs={24}
+                sm={24}
+                md={24}
+                lg={24}
+                xl={24}
+              >
                 <Button
                   type="primary"
                   shape="default"
                   size="large"
-                  icon={<DownloadOutlined />}
                   className={home.btnBatch1}
                 >
-                  CA: 0xF2844304D85037DGJ394805TR129DF
-                  <DownloadOutlined />
+                  <Image width={30} height={30} src="/cat_r.svg" alt="" />
+                  &nbsp;&nbsp;{" "}
+                  <span className={home.textBtnBatch1}>
+                    CA: 0xF2844304D85037DGJ394805TR129DF
+                  </span>{" "}
+                  &nbsp;
+                  <Image width={20} height={20} src="/copy.svg" alt="" />
                 </Button>
               </Col>
             </Row>
-            <Row>
-              <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Row className={home.boxRowBtnBatch2}>
+              <Col
+                className={home.boxColBtnBatch2}
+                xs={24}
+                sm={24}
+                md={12}
+                lg={12}
+                xl={12}
+              >
                 <Button
                   type="primary"
                   shape="default"
                   size="large"
-                  icon={<DownloadOutlined />}
                   className={home.btnBatch2}
                 >
-                  BUY $ROXY
+                  <Image width={30} height={30} src="/thuoc.svg" alt="" />
+                  &nbsp; BUY $ROXY
                 </Button>
               </Col>
-              <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+              <Col
+                className={home.groupBtnBatch3}
+                xs={24}
+                sm={24}
+                md={12}
+                lg={12}
+                xl={12}
+              >
                 <Button
                   type="primary"
                   shape="default"
                   size="large"
-                  icon={"X"}
+                  icon={<Image width={50} height={50} src="/x.svg" alt="" />}
                   className={home.btnBatch3}
-                ></Button>
+                />
                 <Button
                   type="primary"
                   shape="default"
                   size="large"
-                  icon={"X"}
+                  icon={
+                    <Image width={50} height={50} src="/telegram.svg" alt="" />
+                  }
                   className={home.btnBatch3}
-                ></Button>
+                />
                 <Button
                   type="primary"
                   shape="default"
                   size="large"
-                  icon={"X"}
+                  icon={
+                    <Image width={50} height={50} src="/eagle.svg" alt="" />
+                  }
                   className={home.btnBatch3}
-                ></Button>
+                />
               </Col>
             </Row>
           </Col>
@@ -113,32 +149,85 @@ export default function Home() {
               ))}
             </div>
           </Col>
-          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
             <Image
               className={home.imageCat2}
               src="/cat1.svg"
-              width={430}
-              height={412}
+              width={262}
+              height={280}
               alt=""
             />
           </Col>
-          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-            <Title level={4} className={home.textIntroduction}>
-              There are five type of buttons in antd, namely they are: primary
-              buttons, default buttons, dashed buttons, text buttons, and link
-              buttons.
-            </Title>
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            <div className={home.boxIntroduction}>
+              <Image
+                className={home.chuong1}
+                width={15}
+                height={15}
+                src="/chuong1.svg"
+                alt=""
+              />
+              <Image
+                className={home.chuong2}
+                width={35}
+                height={35}
+                src="/chuong2.svg"
+                alt=""
+              />
+              <Title level={4} className={home.textIntroduction}>
+                Meow, chill, and stretch with glee,
+                <br /> Roxy{`'`}s the queen, wild and free.
+                <br /> Crypto world{`'`}s her comfy lap,
+                <br /> Watch her reign, with nary a mishap.
+              </Title>
+              <Image
+                className={home.chuong3}
+                width={45}
+                height={45}
+                src="/chuong3.svg"
+                alt=""
+              />
+              <Image
+                className={home.chuong4}
+                width={35}
+                height={35}
+                src="/chuong4.svg"
+                alt=""
+              />
+            </div>
           </Col>
         </Row>
       </div>
 
       <div className={home.content3}>
         <Row className={home.cartTitleHead}>
-          <Title level={2}>TOKENOMICS</Title>
+          <Image width={350} height={150} src="/TOKENOMICS.svg" alt="" />
         </Row>
         <Row>
           <Col
-            className={home.boxBtnBatch4}
+            style={{ display: "flex", justifyContent: "center" }}
             xs={24}
             sm={24}
             md={24}
@@ -149,10 +238,15 @@ export default function Home() {
               type="primary"
               shape="default"
               size="large"
-              icon={<DownloadOutlined />}
               className={home.btnBatch4}
             >
-              Download DownloadDownload
+              <Image width={30} height={30} src="/cat_r.svg" alt="" />
+              &nbsp;&nbsp;
+              <span className={home.textBtnBatch4}>
+                CA: 0xF2844304D85037DGJ394805TR129DF
+              </span>
+              &nbsp;
+              <Image width={20} height={20} src="/copy.svg" alt="" />
             </Button>
           </Col>
         </Row>
@@ -173,8 +267,8 @@ export default function Home() {
                 height={100}
                 alt=""
               />
-              <Text className={home.cartTitle}>Powered by Solana</Text>
-              <Text className={home.cartText}>10%</Text>
+              <Text className={home.cartTitle}>Supply</Text>
+              <Text className={home.cartText}>1 Billion</Text>
             </div>
           </Col>
           <Col
@@ -193,8 +287,8 @@ export default function Home() {
                 height={100}
                 alt=""
               />
-              <Text className={home.cartTitle}>Powered by Solana</Text>
-              <Text className={home.cartText}>10%</Text>
+              <Text className={home.cartTitle}>LP Burned</Text>
+              <Text className={home.cartText}>100%</Text>
             </div>
           </Col>
           <Col
@@ -213,8 +307,8 @@ export default function Home() {
                 height={100}
                 alt=""
               />
-              <Text className={home.cartTitle}>Powered by Solana</Text>
-              <Text className={home.cartText}>10%</Text>
+              <Text className={home.cartTitle}>Team</Text>
+              <Text className={home.cartText}>2%</Text>
             </div>
           </Col>
           <Col
@@ -233,8 +327,8 @@ export default function Home() {
                 height={100}
                 alt=""
               />
-              <Text className={home.cartTitle}>Powered by Solana</Text>
-              <Text className={home.cartText}>10%</Text>
+              <Text className={home.cartTitle}>Community rewards</Text>
+              <Text className={home.cartText}>5%</Text>
             </div>
           </Col>
         </Row>
@@ -244,13 +338,245 @@ export default function Home() {
         <Col xs={24}>
           <div className={home.boxTextRepeat}>
             {new Array(20).fill(0).map((i, idx) => (
-              <span key={idx}>
+              <span className={home.boxRepeat} key={idx}>
                 <span className={home.textRepeat}>$ROXY</span>
-                <span className={home.textRepeatChild}>o</span>
+                <Image width={20} height={20} src="/cdog_b.svg" alt="" />
               </span>
             ))}
           </div>
         </Col>
+
+        <Row className={home.cartTitleHead}>
+          <Image width={350} height={150} src="/howtobuy.svg" alt="" />
+        </Row>
+
+        <Title level={3} className={home.description}>
+          Follow these easy steps to become a $ROXY holder.
+        </Title>
+
+        <Row className={home.cartContainer2}>
+          <Col
+            className={home.cartContent2}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            <div className={home.cartItem2}>
+              <Text className={home.cartTitleFirst2}>Step1</Text>
+              <Text className={home.cartTitle2}>CREATE A PHANTOM WALLET</Text>
+              <Text className={home.cartText2}>
+                Visit phantom.app and follow the simple steps to create a new
+                account with the Phantom app or browser extension.
+              </Text>
+            </div>
+          </Col>
+          <Col
+            className={home.cartContent2}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            <div className={home.cartItem2}>
+              <Text className={home.cartTitleFirst2}>Step2</Text>
+              <Text className={home.cartTitle2}>Get some $SOL</Text>
+              <Text className={home.cartText2}>
+                Tap the BUY button in the app to purchase $SOL, or deposit $SOL
+                to your Phantom wallet from the crypto exchange of your choice.
+              </Text>
+            </div>
+          </Col>
+          <Col
+            className={home.cartContent2}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            <div className={home.cartItem2}>
+              <Text className={home.cartTitleFirst2}>Step3</Text>
+              <Text className={home.cartTitle2}>
+                Swap $SOL -{">"} $ROXY on PumP.fun
+              </Text>
+              <Text className={home.cartText2}>
+                Visit this link and swap your $SOL for $ROXY
+              </Text>
+            </div>
+          </Col>
+          <Col
+            className={home.cartContent2}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            <div className={home.cartItem2}>
+              <Text className={home.cartTitleFirst2}>Step4</Text>
+              <Text className={home.cartTitle2}>Wait for the PUMP</Text>
+              <Text className={home.cartText2}>
+                HODL tide and join the $ROXY fam
+              </Text>
+            </div>
+          </Col>
+        </Row>
+      </div>
+
+      <div className={home.content5}>
+        <Row className={home.cartTitleHead}>
+          <Image width={450} height={150} src="/MORECUTEROXY.svg" alt="" />
+        </Row>
+
+        <Row className={home.cartContainer3}>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more1.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more2.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={12}
+          >
+            <Image width={200} height={200} src="/cat_more3.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more4.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more4.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more4.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more4.svg" alt="" />
+          </Col>
+          <Col
+            className={home.cartContent3}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Image width={200} height={200} src="/cat_more4.svg" alt="" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "10px 0px",
+            }}
+            xs={24}
+            sm={24}
+            md={24}
+            lg={24}
+            xl={24}
+          >
+            <Button
+              type="primary"
+              shape="default"
+              size="large"
+              className={home.btnBatch1}
+            >
+              <span className={home.textBtnBatch1}>SEE MORE IN TWITTER/X</span>
+            </Button>
+          </Col>
+        </Row>
+
+        <Row className={home.groupBtnBatch3}>
+          <Col>
+            <Button
+              type="primary"
+              shape="default"
+              size="large"
+              icon={<Image width={50} height={50} src="/x.svg" alt="" />}
+              className={home.btnBatch3}
+            />
+            <Button
+              type="primary"
+              shape="default"
+              size="large"
+              icon={<Image width={50} height={45} src="/telegram.svg" alt="" />}
+              className={home.btnBatch3}
+            />
+            <Button
+              type="primary"
+              shape="default"
+              size="large"
+              icon={<Image width={50} height={50} src="/eagle.svg" alt="" />}
+              className={home.btnBatch3}
+            />
+          </Col>
+        </Row>
+
+        <Row style={{ paddingBottom: 30 }}>
+          <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+            <span className={home.textFooter}>
+              2024 $ROXY, All right reserved
+            </span>
+          </Col>
+          <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+            <span className={home.textBoxFooter}>
+              <span className={home.textFooter}>Build with solana</span>
+              <Image width={20} height={20} src="/solana.svg" alt="" />
+            </span>
+          </Col>
+        </Row>
       </div>
     </div>
   );
